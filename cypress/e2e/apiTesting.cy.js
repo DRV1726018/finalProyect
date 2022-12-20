53a96d96f22ca1440693a74ef017970e7d6eeae7
davidrv <davidramirezvillegas@gmail.com>
2022-12-20 13:38:21 -0500

    Status y Ditto

--- cypress/e2e/apiTesting.cy.js
+++ cypress/e2e/apiTesting.cy.js
@@ -1,5 +1,11 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://pokeapi.co/')
+describe('API TEST WITH CYPRESS', () => {
+  it('API Test-Status Validation', () => {
+    cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
+    cy.get('@pokemon').its('status').should('equal', 200)
+  })
+
+  it('API Test-validate Name value', () => {
+    cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
+    cy.get('@pokemon').its('body').should('include', { name: 'ditto' })
   })
 })
