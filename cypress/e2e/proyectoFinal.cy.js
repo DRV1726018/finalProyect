66647fe5d0c046696ce7a238f6eb96ed282e8d27
davidrv <davidramirezvillegas@gmail.com>
2022-12-22 12:56:16 -0500

    Four test cases created

--- cypress/e2e/proyectoFinal.cy.js
+++ cypress/e2e/proyectoFinal.cy.js
@@ -1,10 +1,13 @@
 import { LandingPageSGS6 } from "../pages/landingPage"
 import { LandingPageSV } from "../pagesLaptop/landingPage"
 import { LandingPageAM } from "../pageMonitor/landingPage"
+import { LandingPageNL } from "../pageDeletedPhone/landingPage"
+import { DeletedPhone } from "../pageDeletedPhone/deletedPhone"
 describe('UI TESTING WITH CYPRESS', () => {
   const expectedPhone = 'Samsung galaxy s6'
   const expectedLaptop = 'Sony vaio i5'
   const expectedMonitor = 'Apple monitor 24'
+  const expectedDeletedPhone = 'Nokia lumia 1520'
   it('Adding Samsung galaxy s6 to the cart', () => {
     const home = new LandingPageSGS6
     const SamsungGalaxyS6 = home.clickSamsungGalaxyS6()
@@ -25,5 +28,15 @@ describe('UI TESTING WITH CYPRESS', () => {
     AppleMonitor.clickAddToCart()
     const Cart = AppleMonitor.goToCart()
     Cart.checkFirstElementName(expectedMonitor)
+  })
+  it('Deleting a Nokia Lumia 1520 from the cart', () => {
+    const home = new LandingPageNL
+    const NokiaLumia = home.clickNokiaLumia()
+    NokiaLumia.clickAddToCart()
+    const Cart = NokiaLumia.goToCart()
+    Cart.checkFirstElementName(expectedDeletedPhone)
+    const deleted = new DeletedPhone
+    deleted.DeletedPhone()
+    deleted.checkDeletedPhone()
   })
 })
